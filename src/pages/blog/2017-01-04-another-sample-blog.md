61ec6088802c716db9adb060d1d4513df014c5eb
Zack Singh <zacharysingh@gmail.com>
2020-06-11 16:13:55 -0700

    Implement formatting fixes to homepagee

---
+++ src/pages/blog/2017-01-04-another-sample-blog.md
@@ -0,0 +1,35 @@
+---
+templateKey: 'blog-post'
+title: 'This is a sample blog title of a featured blog'
+date: 2017-01-04T15:04:10.000Z
+featuredpost: true
+description: >-
+  This is a sample description of this blog post.
+tags:
+  - sampletag1
+  - sample tag 2
+  - birth
+  - doulaing
+---
+
+A couple sample paragraphs here.
+
+A couple sample paragraphs here.
+
+Very exciting stuff.
+
+## A sample header
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum erat dictum ipsum vehicula dictum. Duis at mi ultrices, ornare sem tempor, tincidunt arcu.
+
+>Sample indentation. Very very exciting.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum erat dictum ipsum vehicula dictum. Duis at mi ultrices, ornare sem tempor, tincidunt arcu.
+
+## Another sample header
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum erat dictum ipsum vehicula dictum. Duis at mi ultrices, ornare sem tempor, tincidunt arcu.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum erat dictum ipsum vehicula dictum. Duis at mi ultrices, ornare sem tempor, tincidunt arcu.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum erat dictum ipsum vehicula dictum. Duis at mi ultrices, ornare sem tempor, tincidunt arcu.
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -32,16 +32,16 @@ export const IndexPageTemplate = ({
           height: '150px',
           lineHeight: '1',
           justifyContent: 'space-around',
-          alignItems: 'left',
+          alignItems: 'center',
           flexDirection: 'column',
         }}
       >
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            // boxShadow:
+            //   'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+            // backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -52,9 +52,9 @@ export const IndexPageTemplate = ({
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            // boxShadow:
+            //   'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+            // backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
